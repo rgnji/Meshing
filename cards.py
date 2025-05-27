27a5a3f5f7a8fdc89be7e84f8acdca7a2c68f678
rgnji <xx141463@gmail.com>
2025-05-27 17:47:26 +0800

    group 15 issue again

--- cards.py
+++ cards.py
@@ -496,6 +496,10 @@ with open("fort11.txt", "w", encoding="UTF-8") as f:
 
     # ================== group 15 ==================
     group15 = ['ISPARK','ISPKMIN','ISPMAX']
+    for g15 in group15:
+        f.write(f'{g15:>6},')
+    f.write('\n')
+    f.write(f'{0:>6},{0:>6},{0:>6}\n')
 
     # ================== group 16 ==================
     group16 = ['ISPKON','ISPKZN','ISPKI1','ISPKIM','ISPKJ1','ISPKJM','ISPKK1','ISPKKM','TMSPK','ISPKDBG']
